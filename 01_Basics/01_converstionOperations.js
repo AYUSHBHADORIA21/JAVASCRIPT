0b5b70e23d36aa2673d24e373610d52d943a3311
AYUSHBHADORIA21 <ayushbhadoriya1241@gmail.com>
2024-06-04 20:37:35 +0000

    converstion

---
+++ 01_Basics/01_converstionOperations.js
@@ -0,0 +1,19 @@
+let score   ="ayush"
+
+let valueNumber = Number(score);
+
+console.log(valueNumber);
+console.log(typeof(valueNumber));
+
+//"33" => 33
+//"33abc" => NAN
+//true = 1 , false = 0 ; 
+
+let isloggedin = "ayush"
+
+let booleanIsLogedIn = Boolean(isloggedin)
+console.log(booleanIsLogedIn)
+
+// 1 => True  ,  0 =>False
+// "" => false
+//"ayush" => true
