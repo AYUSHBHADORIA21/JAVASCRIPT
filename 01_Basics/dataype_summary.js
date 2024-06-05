9295475871e58906f9aaa0e68a14230805898d5b
AYUSHBHADORIA21 <ayushbhadoriya1241@gmail.com>
2024-06-05 12:00:52 +0000

    heap and stack

--- 01_Basics/dataype_summary.js
+++ 01_Basics/dataype_summary.js
@@ -13,3 +13,38 @@
 
 //Array , Object , Functions
 
+
+//```````````````````````````````````````````````````````````````````````````````````````````````
+
+//stack(Primitive) and heap(Non-Primitive)
+
+let MyOccupation = "Enginner";
+
+let NewOccupation = MyOccupation;
+NewOccupation = "Leiutenant"
+
+//ab ye stack me value store karega mtlb Newocupation Copy store karega Myoccupation ki
+//agar NewOccupation me change karenge to MyOccupation me change nahi hoga kyu ki 
+//hamne wake by reference nahi uthae he copy uthae he MyOccuptaion ki 
+
+let UserOne = {
+    name:"ayush",
+    rollNo:21
+}
+
+let UserTwo = UserOne;
+
+UserTwo.rollNo = 29 ; 
+
+console.log(UserOne);
+console.log(UserTwo);
+
+//edhar by reference call hoga thik mtlb ye he ke isme 
+//ek to heap me value store hogi upar se by reference call hoga sab kuch 
+//agar UserOne point kar rha he ek value ko tu UserTwo bhi point karega uski value ko 
+//value me change mtlb dono users me change 
+
+
+//UserOne(name) hoga to stack me save par uski value jaega Heap me udhar se by reference call hoga
+
+
